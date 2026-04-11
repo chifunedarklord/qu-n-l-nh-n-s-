bd294bd52e178c090cf6be027ad4b67603ca2188
chúa tể bóng tối <soihoanghh2005@gmail.com>
2026-04-11 14:48:09 +0700

    Delete quyanlynhansu directory

--- quyanlynhansu/exceptions/__init__.py
+++
@@ -1,8 +0,0 @@
-from .employee_exceptions import (
-    EmployeeException,
-    EmployeeNotFoundError,
-    InvalidSalaryError,
-    InvalidAgeError,
-    ProjectAllocationError,
-    DuplicateEmployeeError
-)
--- quyanlynhansu/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .validators import validate_age, validate_salary, validate_email, validate_performance_score
-from .formatters import format_currency, format_employee_info, format_separator, format_title
